bc00f20279e2b02836ea3f23ff70bc5a32a7ab05
Brian Lawi Ochiel <lawioochiel@gmail.com>
2023-10-19 19:22:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+// removing node lements 
+document.querySelector('main').remove()
+
+//  Creating of a new node and instertion of text 
+ const newHeader = document.createElement('h1');
+ newHeader.id = 'victory';
+ newHeader.innerHTML ="Lawi is the champion";
+ 
